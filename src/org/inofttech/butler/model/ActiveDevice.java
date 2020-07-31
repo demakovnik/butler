e6b6b16c488b8ed54508f762dd3c0e9607c88a4f
demakovnik <Nortumbria1986_>
2020-07-31 23:22:04 +0700

    started implementing the domain model

--- src/org/inofttech/butler/model/ActiveDevice.java
+++ src/org/inofttech/butler/model/ActiveDevice.java
@@ -1,4 +1,38 @@
 package org.inofttech.butler.model;
 
-public abstract class ActiveDevice {
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "activedevices")
+public class ActiveDevice extends AbstractNamedEntity {
+
+    protected String description;
+    protected String networkAddress;
+
+    public ActiveDevice() {
+    }
+
+    public ActiveDevice(Long id, String name, String description, String networkAddress) {
+        super(id, name);
+        this.description = description;
+        this.networkAddress = networkAddress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNetworkAddress() {
+        return networkAddress;
+    }
+
+    public void setNetworkAddress(String address) {
+        this.networkAddress = address;
+    }
 }
